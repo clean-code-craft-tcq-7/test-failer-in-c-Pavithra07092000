cc2def7755c3e58c8160e31bd96846f1730b4c36
PavithraRavichandran595 <paviravichandran595@gmail.com>
2025-08-03 09:19:16 +0530

    fix the code

--- tshirts.c
+++ tshirts.c
@@ -1,8 +1,9 @@
+
 #include <stdio.h>
 #include <assert.h>
 
 char size(int cms) {
-    char sizeName = '\0';
+    char sizeName = '\0';  //Initializes the result with a null character
     if (cms < 38) {
         sizeName = 'S';
     } else if (cms > 38 && cms < 43) {
@@ -15,10 +16,22 @@ char size(int cms) {
 
 int testTshirtSize() {
     printf("\nTshirt size test\n");
+
     assert(size(37) == 'S');
     assert(size(40) == 'M');
+    assert(size(44) == 'L');
+    assert(size(38) == 'S');
+    assert(size(42) == 'M');
     assert(size(43) == 'L');
+    assert(size(0) == '\0');
+    assert(size(50) == 'L');
+
     printf("All is well (maybe!)\n");
+    return 0;
+}
+
+int main() {
+    testTshirtSize();
     return 0;
 }
 
