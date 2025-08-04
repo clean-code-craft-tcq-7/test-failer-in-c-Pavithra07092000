ca1872b0d6743c7e934f1819b5ec0cd90c12fc07
PavithraRavichandran595 <paviravichandran595@gmail.com>
2025-08-04 17:00:48 +0530

    fix the code

--- misaligned.c
+++ misaligned.c
@@ -4,13 +4,13 @@
 int printColorMap() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
-    int i = 0, j = 0;
-    for (i = 0; i < 5; i++) {
-        for (j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+    int i = 1, j = 1;
+    for (i = 1; i < 5; i++) {
+        for (j = 1; j < 5; j++) {
+            printf("%d | %s | %s\n", ((i-1) * 5) + (j-1), majorColor[i-1], minorColor[j-1]);
         }
     }
-    return i * j;
+    return (i-1) * (j-1);
 }
 
 int testPrintColorMap() {
